ceb4a9c407c83266e4eab9c2bf34593ba46ff346
rsbyrne <rohan.byrne@gmail.com>
2021-02-17 06:31:10 +0000

    Fixed an oversight in .ignore files.

--- it.sh
+++ it.sh
@@ -5,6 +5,5 @@ MOUNTFROM=$PWD
 MOUNTTO='/home/morpheus/workspace/mount'
 IMAGE='rsbyrne/thesis'
 SOCK='/var/run/docker.sock'
-docker run -v $MOUNTFROM:$MOUNTTO -v $SOCK:$SOCK --shm-size 2g -p 8888:8888 $IMAGE \
-  jupyter lab --no-browser --allow-root --port=8888 --ip='0.0.0.0'
+docker run -v $MOUNTFROM:$MOUNTTO -v $SOCK:$SOCK -it --shm-size 2g $IMAGE bash
 cd $currentDir
