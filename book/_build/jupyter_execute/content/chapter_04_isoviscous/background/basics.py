6f4040a16072f41d22dad42b19bebdf8210e8341
rsbyrne <rohan.byrne@gmail.com>
2021-06-01 18:48:58 +1000

    Quick push

--- book/_build/jupyter_execute/content/chapter_04_isoviscous/background/basics.py
+++ book/_build/jupyter_execute/content/chapter_04_isoviscous/background/basics.py
@@ -50,8 +50,13 @@
 # 
 # Within these simplifying constraints, almost limitless variety is possible - which is why this essential formulation has become common to virtually all studies of mantle convection. However, while it is hoped that somewhere within problem-space a solution resembling Earth may be found, such a solution must elude us until our grasp of the fundamentals is absolute; and there is still much about even the simplest rheologies that we do not understand.
 
-# In[ ]:
-
-
-
-
+# ## Linear rheologies: the isoviscous case
+# 
+# When we talk about rheology, or a materials' style of flow, we are primarily talking about its viscosity function - the expression that maps the local viscosity at any point in the fluid to its context, history, or environment. While most fluids in the real world have complex 'Non-Newtonian' rheologies in which pressure and viscosity are co-dependent, mantle convection studies have often relied on simpler Newtonian rheologies that are more numerically and analytically tractable. These are what we here call 'linear rheologies'.
+# 
+# The bulk of all mantle convection studies focus on one of the following three linear rheologies:
+# - **Isoviscous** or 'constant viscosity' flow.
+# - **Arrhenius**-type exponentially temperature-dependent viscosity.
+# - **Spatialised** viscosity - either arbitrarily zoned or simply depth-dependent.
+# 
+# This two-part investigation will focus on the isoviscous and Arrhenius types. The former can be viewed as a special case of the latter, so it makes a fitting launching-off point for this study.
