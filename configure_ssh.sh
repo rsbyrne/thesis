d829a1d393edd01d5cc4834e09ed337b8c85198b
rsbyrne <rohan.byrne@gmail.com>
2025-05-05 01:05:05 +0000

    Quick push

--- configure_ssh.sh
+++ configure_ssh.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 currentDir=$PWD
 cd "$(dirname "$0")"
-mkdir ~/.ssh/
+# mkdir ~/.ssh/
 sudo rm -r ~/.ssh/*
 cp -f *.pem ~/.ssh/
 sudo chmod 700 ~/.ssh/
 sudo chmod 600 ~/.ssh/*
-# eval "$(ssh-agent)"
-# ssh-add ~/.ssh/*.pem
+eval "$(ssh-agent)"
+ssh-add ~/.ssh/*.pem
 git config --global url.ssh://git@github.com/.insteadOf https://github.com/
 cd $currentDir
